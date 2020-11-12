b830991e1a1a11c25667988a1608ad0abdfbd40a
shoowack <isuvak@gmail.com>
2020-11-12 09:57:22 -0500

    moving navigation component

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { HashLink as Link } from "react-router-hash-link";
 
 export default function Navigation({ scrollWithOffset }) {
+  const [isNavCollapsed, setIsNavCollapsed] = useState(true);
+
+  const handleNavCollapse = (el) => {
+    if (el === undefined) {
+      setIsNavCollapsed(true);
+    } else if (el.target.className === "navbar-toggler-icon") {
+      setIsNavCollapsed(!isNavCollapsed);
+    }
+  };
+
   return (
     <nav
-      className={`navbar navbar-expand-lg fixed-top bg-light navbar-light`} // bg-transparent navbar-dark
+      className="navbar navbar-expand-lg fixed-top bg-dark navbar-dark" // bg-transparent navbar-dark
     >
       <div className="container">
         <Link
@@ -13,7 +23,34 @@ export default function Navigation({ scrollWithOffset }) {
           to="/#top"
           onClick={() => handleNavCollapse()}
           scroll={(el) => scrollWithOffset(el)}
-        ></Link>
+        >
+          Home
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          onClick={(el) => handleNavCollapse(el)}
+          aria-controls="navbarText"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className={`${isNavCollapsed ? "collapse" : ""} navbar-collapse`}>
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <Link
+                className="nav-link"
+                smooth
+                to="/#download"
+                onClick={() => handleNavCollapse()}
+                scroll={(el) => scrollWithOffset(el)}
+              >
+                Island Rab
+              </Link>
+            </li>
+          </ul>
+        </div>
       </div>
     </nav>
   );
