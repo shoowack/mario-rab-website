9451eecf61b92cbcf90a583407cee7c432c4b923
shoowack <isuvak@gmail.com>
2020-11-15 18:09:58 -0500

    trying out google maps component, creating page wrapper component

---
+++ src/components/page-wrapper.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import GoogleMapReact from "google-map-react";
+import "./page-wrapper.scss";
+
+export default function PageWrapper({ children, headerImage }) {
+  const loc = {
+    center: {
+      lat: 44.828537,
+      lng: 14.748109
+    },
+    marker: {
+      lat: 44.828537,
+      lng: 14.748109
+    },
+    zoom: 11
+  };
+
+  const AnyReactComponent = ({ text }) => <div>{text}</div>;
+
+  return (
+    <div id="core" className="page-property-details">
+      <div
+        className="page-header"
+        style={{ background: `url(${headerImage})` }}
+      >
+        <div className="container">
+          <div className="page-header-inner"></div>
+        </div>
+      </div>
+      <div className="main-wrapper-container">
+        <div className="container">
+          <div id="main-wrapper">
+            <div className="island">
+              <div style={{ height: "200px", width: "100%" }}>
+                <GoogleMapReact
+                  bootstrapURLKeys={{ key: process.env.REACT_APP_MAP_KEY }}
+                  defaultCenter={loc.center}
+                  defaultZoom={loc.zoom}
+                >
+                  <AnyReactComponent
+                    lat={loc.marker.lat}
+                    lng={loc.marker.lng}
+                    text="My Marker"
+                  ></AnyReactComponent>
+                </GoogleMapReact>
+              </div>
+            </div>
+            {children}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
