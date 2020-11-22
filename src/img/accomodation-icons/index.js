8fd37976b7af3c38d1cf0bb3d05768d7cb18ab66
shoowack <isuvak@gmail.com>
2020-11-22 15:59:34 -0500

    fixing some styles, errors, added accomodation icons

---
+++ src/img/accomodation-icons/index.js
@@ -0,0 +1,10 @@
+import Aircon from "./aircon.svg";
+import BathTub from "./bath.svg";
+import BBQ from "./bbq.svg";
+import Bed from "./bedroom.svg";
+import Steps from "./floor.svg";
+import ParkingSpot from "./parking.svg";
+import Washer from "./washer.svg";
+import WiFi from "./wifi.svg";
+
+export { Aircon, BathTub, BBQ, Bed, Steps, ParkingSpot, Washer, WiFi };
