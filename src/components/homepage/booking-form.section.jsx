3361e4329a17f6cb821adb43601685de4f6ef774
shoowack <isuvak@gmail.com>
2020-11-26 09:17:55 -0500

    styling booking form

--- src/components/homepage/booking-form.section.jsx
+++ src/components/homepage/booking-form.section.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Container, Col, Form, Button } from "react-bootstrap";
+import { Container, Row, Col, Form, Button } from "react-bootstrap";
 import "./booking-form.scss";
 
 const encode = (data) => {
@@ -31,64 +31,66 @@ export default function BookingFormSection() {
   return (
     <div id="banner" style={{ height: "500px" }}>
       <Container>
-        <div className="banner-search px-md-5 py-md-4 w-100">
-          <Form onSubmit={handleSubmit}>
-            <Form.Group
-              as={Col}
-              sm={12}
-              md={4}
-              className="m-0 px-4 px-md-2 py-2 py-md-0"
-            >
-              <Form.Label className="ml-2">Email</Form.Label>
-              <Form.Control
-                type="email"
-                placeholder="Enter email"
-                required
-                name="email"
-                value={email}
-                onChange={(e) => handleChange(e)}
-              />
-            </Form.Group>
-            <Form.Group
-              as={Col}
-              sm={12}
-              md={3}
-              className="m-0 px-4 px-md-2 py-2 py-md-0"
-            >
-              <Form.Label className="ml-2">Arrival</Form.Label>
-              <Form.Control
-                type="date"
-                name="arrival"
-                value={arrival}
-                onChange={(e) => handleChange(e)}
-              />
-            </Form.Group>
-            <Form.Group
-              as={Col}
-              sm={12}
-              md={3}
-              className="m-0 px-4 px-md-2 py-2 py-md-0"
-            >
-              <Form.Label className="ml-2">Departure</Form.Label>
-              <Form.Control
-                type="date"
-                name="departure"
-                value={departure}
-                onChange={(e) => handleChange(e)}
-              />
-            </Form.Group>
-            <Form.Group
-              as={Col}
-              sm={12}
-              md={2}
-              className="m-0 px-4 px-md-2 py-3 py-md-0 text-center"
-            >
-              <Button type="submit">
-                <i className="fa fa-send"></i> string:make-reservation
-              </Button>
-            </Form.Group>
+        <Row className="banner-search px-md-5 py-md-4">
+          <Form onSubmit={handleSubmit} className="col">
+            <Row>
+              <Form.Group
+                as={Col}
+                sm={12}
+                md={4}
+                className="m-0 px-4 px-md-2 py-2 py-md-0"
+              >
+                <Form.Label className="ml-2">Email</Form.Label>
+                <Form.Control
+                  type="email"
+                  placeholder="Enter email"
+                  required
+                  name="email"
+                  value={email}
+                  onChange={(e) => handleChange(e)}
+                />
+              </Form.Group>
+              <Form.Group
+                as={Col}
+                sm={12}
+                md={3}
+                className="m-0 px-4 px-md-2 py-2 py-md-0"
+              >
+                <Form.Label className="ml-2">Arrival</Form.Label>
+                <Form.Control
+                  type="date"
+                  name="arrival"
+                  value={arrival}
+                  onChange={(e) => handleChange(e)}
+                />
+              </Form.Group>
+              <Form.Group
+                as={Col}
+                sm={12}
+                md={3}
+                className="m-0 px-4 px-md-2 py-2 py-md-0"
+              >
+                <Form.Label className="ml-2">Departure</Form.Label>
+                <Form.Control
+                  type="date"
+                  name="departure"
+                  value={departure}
+                  onChange={(e) => handleChange(e)}
+                />
+              </Form.Group>
+              <Form.Group
+                as={Col}
+                sm={12}
+                md={2}
+                className="m-0 px-4 px-md-2 py-3 py-md-0 text-center"
+              >
+                <Button type="submit">
+                  <i className="fa fa-send"></i> string:make-reservation
+                </Button>
+              </Form.Group>
+            </Row>
           </Form>
-        </div>
+        </Row>
       </Container>
     </div>
   );
