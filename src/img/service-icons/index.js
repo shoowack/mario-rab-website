e547d4ab175ef32a269317cb512b14e3a9d29d2b
shoowack <isuvak@gmail.com>
2020-12-02 21:33:36 -0500

    moved service icons to img folder

---
+++ src/img/service-icons/index.js
@@ -0,0 +1,8 @@
+import Beaches from "./beaches.svg";
+import Fun from "./fun.svg";
+import ActiveHolidays from "./active-holidays.svg";
+import GastronomicOffer from "./gastronomic-offer.svg";
+import History from "./history.svg";
+import Arrival from "./arrival.svg";
+
+export { Beaches, Fun, ActiveHolidays, GastronomicOffer, History, Arrival };
